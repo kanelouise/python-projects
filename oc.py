18cbff5c6aa184f8c26416fa1cc0a17fd916f9c8
kane <kanelouise@users.noreply.github.com>
2020-03-27 17:06:44 -0400

    Add files via upload

---
+++ oc.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#create dataframe of polling locations from LA County website
+oc = pd.read_html('https://ocvote.com/fileadmin/vc/vclist.html')[1]
+
+#test to make sure the correct table was selected
+#print(oc)
+
+#output dataframe to csv
+oc.to_csv("oc_pls_from_county_site_2020-02-28.csv", index=False)
